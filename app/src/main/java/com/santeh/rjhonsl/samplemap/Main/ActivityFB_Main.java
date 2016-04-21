c792c4129c3433578d6216621c9566183e5bdc1c
rjhonsl <r.jhonsl.personal@gmail.com>
2016-04-21 16:19:58 +0800

    **Adjustments            -Fishbook MainTemplate

--- app/src/main/java/com/santeh/rjhonsl/samplemap/Main/ActivityFB_Main.java
+++ app/src/main/java/com/santeh/rjhonsl/samplemap/Main/ActivityFB_Main.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -24,12 +26,22 @@ import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.santeh.rjhonsl.samplemap.R;
 import com.santeh.rjhonsl.samplemap.Utils.Helper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 /**
  * Created by rjhonsl on 4/14/2016.
@@ -41,6 +53,16 @@ public class ActivityFB_Main extends AppCompatActivity {
     ImageView imageview;
     ImageButton btnPopupOptions;
     String finalHashtag;
+    ScrollView scrollView;
+    private static final int SELECT_PICTURE = 1;
+    private static final int SELECT_FILE = 2;
+    private String selectedFilePath;
+
+    FloatingActionButton fabAddPost;
+    LinearLayout llbottomwrapper;
+
+    Boolean isBottomAnimating = false;
+    Boolean isFabSelected = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,11 +79,50 @@ public class ActivityFB_Main extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
 
 
-
+        fabAddPost = (FloatingActionButton) findViewById(R.id.fab_addpost);
+        scrollView = (ScrollView) findViewById(R.id.scrollView_fbmain);
+        llbottomwrapper = (LinearLayout) findViewById(R.id.ll_wrapper_bottom);
         CardView cardview1 = (CardView) findViewById(R.id.post1);
         TextView txtLinkToComments1 = (TextView) cardview1.findViewById(R.id.txtlinkcomments);
-        TextView txtlinktoShare1 = (TextView) cardview1.findViewById(R.id.txtlinkshare);
         final ImageButton btn1 = (ImageButton) cardview1.findViewById(R.id.btnPostOptions);
+
+        LinearLayout llpostSomething = (LinearLayout) findViewById(R.id.ll_postSomething);
+        LinearLayout lluploadPhoto = (LinearLayout) findViewById(R.id.ll_uploadPhoto);
+        LinearLayout lluploadFile = (LinearLayout) findViewById(R.id.ll_uploadfile);
+        LinearLayout llshare = (LinearLayout) cardview1.findViewById(R.id.ll_share);
+
+        llshare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Text I want to share.";
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                share.putExtra(Intent.EXTRA_TEXT, message);
+
+                startActivity(Intent.createChooser(share, "Share to:"));
+            }
+        });
+
+        llpostSomething.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        lluploadPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        lluploadFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
         txtLinkToComments1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -151,8 +212,6 @@ public class ActivityFB_Main extends AppCompatActivity {
         txtpost.setHighlightColor(Color.TRANSPARENT);
 
 
-
-
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -211,25 +270,158 @@ public class ActivityFB_Main extends AppCompatActivity {
             }
         });
 
-        // Enable the Up button
+
         ab.setDisplayHomeAsUpEnabled(true);
 
-//        getSupportActionBar().setHomeButtonEnabled(true);
-//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
-//        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setCustomView(R.menu.menu_search);
+
+
+//        scrollView.setOnGenericMotionListener(new View.OnGenericMotionListener() {
+//            @Override
+//            public boolean onGenericMotion(View v, MotionEvent event) {
+//                if (llbottomwrapper.getVisibility() == View.VISIBLE) {
+////                    llbottomwrapper.setVisibility(View.GONE);
+////                    fabAddPost.setVisibility(View.VISIBLE);
+//                    isFabSelected = false;
+//                    animateBottom();
+//                }
+//                return false;
+//            }
+//        });
+
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                if (llbottomwrapper.getVisibility() == View.VISIBLE) {
+//                    llbottomwrapper.setVisibility(View.GONE);
+//                    fabAddPost.setVisibility(View.VISIBLE);
+                    isFabSelected = false;
+                    if (!isBottomAnimating){
+                        animateBottom();
+                    }
+
+                }
+            }
+        });
+
+        fabAddPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if (llbottomwrapper.getVisibility() == View.VISIBLE) {
+////                    llbottomwrapper.setVisibility(View.GONE);
+////                    fabAddPost.setVisibility(View.VISIBLE);
+                isFabSelected = true;
+
+                animateBottom();
+//                } else {
+////                    llbottomwrapper.setVisibility(View.VISIBLE);
+////                    fabAddPost.setVisibility(View.GONE);
+//                    isFabSelected = false;
+//                    animateBottom();
+//                }
+            }
+        });
+
+        lluploadPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+            }
+        });
+
+
+
+        lluploadFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                String[] mimetypes = {
+                        "text/*",
+                        "application/msword",
+                        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                        "application/vnd.ms-excel",
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        "application/vnd.ms-powerpoint",
+                        "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                        "application/x-rar-compressed",
+                        "application/zip"
+                };
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_FILE);
+            }
+        });
+
 
     }
+
+    private void animateBottom(){
+        Animation animatelayout;
+        int visibility;
+
+        if (isFabSelected){
+            animatelayout  = AnimationUtils.loadAnimation(context, R.anim.bottom_up);
+            animatelayout.setDuration(300);
+            animatelayout.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    llbottomwrapper.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+            visibility = View.VISIBLE;
+
+            fabAddPost.setVisibility(View.GONE);
+            llbottomwrapper.startAnimation(animatelayout);
+
+        }
+        else{
+            animatelayout  = AnimationUtils.loadAnimation(context, R.anim.bottom_down);
+            animatelayout.setDuration(300);
+            animatelayout.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    isBottomAnimating = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    isBottomAnimating = false;
+                    llbottomwrapper.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            fabAddPost.setVisibility(View.VISIBLE);
+            llbottomwrapper.startAnimation(animatelayout);
+        }
+
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_search, menu);
-//        MenuItem searchItem = menu.findItem(R.id.action_search);
-//        SearchView searchView =
-//                (SearchView) MenuItemCompat.getActionView(searchItem);
 
         // Define the listener
         MenuItemCompat.OnActionExpandListener expandListener = new MenuItemCompat.OnActionExpandListener() {
@@ -253,5 +445,34 @@ public class ActivityFB_Main extends AppCompatActivity {
         MenuItemCompat.setOnActionExpandListener(actionMenuItem, expandListener);
 
         return true;
+    }
+
+
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE || requestCode == SELECT_FILE) {
+                Uri selectedFileUri = data.getData();
+
+                File file = getPath(selectedFileUri);
+                selectedFilePath = file.getAbsolutePath();
+
+                Helper.toastLong(activity, selectedFilePath);
+                try {
+                    FileInputStream is = new FileInputStream(file);
+                    FileOutputStream os = new FileOutputStream(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+    public File getPath(Uri uri) {
+        return new File(uri.getPath());
     }
 }
